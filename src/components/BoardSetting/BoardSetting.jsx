cdf5fa1acd2280cb70bd24f5d50a3a9ffa469150
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-07-22 21:34:09 +0900

    Feat: 좌클릭시 보드판의 셀을 뒤집을 수 있는 기능 추가

--- src/components/BoardSetting/BoardSetting.jsx
+++ src/components/BoardSetting/BoardSetting.jsx
@@ -1,21 +1,70 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { createMine } from "../../utils/createMine";
 import { CELL } from "../../constant/constant";
 import { useSelector, useDispatch } from "react-redux";
-import { startGame } from "../../store/boardSlice";
+import { startGame, openCell, clickedMine } from "../../store/boardSlice";
 const Td = styled.td`
   width: 40px;
   height: 40px;
   border: 1px solid black;
+  color: #fff;
+  background-color: ${({ cellData }) => {
+    switch (cellData) {
+      case 0:
+        return "#fff";
+      case -1:
+      case -7:
+        return "#000";
+      default:
+        return "#fff";
+    }
+  }};
 `;
 export default function BoardSetting() {
   const [row, setRow] = useState(10);
   const [col, setCol] = useState(10);
   const [mine, setMine] = useState(20);
   const boardData = useSelector((state) => state.board.boardData);
-  console.log(boardData);
   const dispatch = useDispatch();
+  console.log(boardData);
+
+  const getText = (code) => {
+    switch (code) {
+      case CELL.NORMAL:
+        return "";
+      case CELL.MINE:
+        return "X";
+      case CELL.CLICKED_MINE:
+        return "💣";
+      case CELL.FLAG_MINE:
+      case CELL.FLAG:
+        return "🚩";
+      case CELL.QUESTION_MINE:
+      case CELL.QUESTION:
+        return "❓";
+      default:
+        return code || "";
+    }
+  };
+
+  const onLeftClick = (rowIndex, colIndex) => {
+    switch (boardData[rowIndex][colIndex]) {
+      case CELL.OPENED:
+      case CELL.FLAG:
+      case CELL.FLAG_MINE:
+      case CELL.QUESTION:
+      case CELL.QUESTION_MINE:
+        return;
+      case CELL.NORMAL:
+        dispatch(openCell({ rowIndex, colIndex }));
+        return;
+      case CELL.MINE:
+        dispatch(clickedMine({ rowIndex, colIndex }));
+        return;
+      default:
+        return;
+    }
+  };
 
   return (
     <>
@@ -56,7 +105,13 @@ export default function BoardSetting() {
             boardData.map((row, rowIndex) => (
               <tr key={rowIndex}>
                 {row.map((col, colIndex) => (
-                  <Td key={colIndex}>{col === CELL.MINE ? "-7" : "-1"}</Td>
+                  <Td
+                    key={colIndex}
+                    cellData={col}
+                    onClick={() => onLeftClick(rowIndex, colIndex)}
+                  >
+                    {getText(col)}
+                  </Td>
                 ))}
               </tr>
             ))}
--- src/store/boardSlice.js
+++ src/store/boardSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { createMine } from '../utils/createMine';
+import { CELL } from '../constant/constant';
 
 const initialState = {
   boardData: [],
@@ -8,22 +9,36 @@ const initialState = {
     col: 0,
     mine: 0,
   },
+  stop: false,
 }
 
 export const boardSlice = createSlice({
   name: 'board',
   initialState,
   reducers: {
-    startGame: (state, { payload: { row, col, mine } }) => {
+    startGame: (state, action) => {
+      const { row, col, mine } = action.payload;
       state.data.row = row;
       state.data.col = col;
       state.data.mine = mine;
       const newBoardData = createMine(row, col, mine);
       state.boardData = [...newBoardData];
+      state.stop = false;
     },
+    openCell: (state, action) => {
+      const { rowIndex, colIndex } = action.payload;
+      const boardData = [...state.boardData];
+      boardData[rowIndex][colIndex] = CELL.OPENED;
+    },
+    clickedMine: (state, action) => {
+      const { rowIndex, colIndex } = action.payload;
+      const boardData = [...state.boardData];
+      boardData[rowIndex][colIndex] = CELL.CLICKED_MINE;
+      state.stop = true;
+    }
   },
 })
 
-export const { startGame } = boardSlice.actions
+export const { startGame, openCell, clickedMine } = boardSlice.actions
 
 export default boardSlice.reducer
